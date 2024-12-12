1b3766d83912a2d0fe0faa834c71fcf52262969b
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-12 22:45:02 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { Hono } from 'hono/quick'
 
 const app = new Hono()
-app.get('/', c => c.text('Hello Bun!'))
+// log requests
+app.use(logger())
+// handle errors
+app.onError((err, c) => {
+  console.error(err)
+  return c.text('An unexpected error occurred', 500)
+})
+
+app.post('/png', async c => {
+  // get text
+  const text = await c.req.text()
+  // log text
+  console.info(text)
+  // return 200
+  return c.text('ok')
+})
 
 export default app
