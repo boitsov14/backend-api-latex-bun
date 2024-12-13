7dfd3b916f53a0f2c99a3670ac77a8ecf1efc011
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-13 21:45:31 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { mkdtempSync, rmSync } from 'node:fs'
 import { createMiddleware } from 'hono/factory'
 import { logger } from 'hono/logger'
 import { Hono } from 'hono/quick'
+import { z } from 'zod'
 
 const app = new Hono()
 // log requests
@@ -12,9 +13,9 @@ app.onError((err, c) => {
   return c.text('An unexpected error occurred', 500)
 })
 // create temp dir
-app.use(
-  // biome-ignore lint/style/useNamingConvention:
-  createMiddleware<{ Variables: { out: string } }>(async (c, next) => {
+// biome-ignore lint/style/useNamingConvention:
+const tempDirMiddleware = createMiddleware<{ Variables: { out: string } }>(
+  async (c, next) => {
     // create temp dir
     const out = mkdtempSync('out-')
     try {
@@ -24,13 +25,16 @@ app.use(
       // remove temp dir
       rmSync(out)
     }
-  }),
+  },
 )
-app.post('/png', async c => {
-  // get text
-  const text = await c.req.text()
-  // log text
-  console.info(text)
+
+app.post('/png', tempDirMiddleware, async c => {
+  // get multipart data
+  const body = await c.req.parseBody()
+  // get file
+  const { file } = z.object({ file: z.instanceof(File) }).parse(body)
+  // save file to temp directory
+  await Bun.write(`${c.get('out')}/${file.name}`, await file.arrayBuffer())
   // return 200
   return c.text('ok')
 })
