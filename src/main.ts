03ae9ef007bbf18069708d4e021ecc3181b26657
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-12-13 21:19:23 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { mkdtempSync, rmSync } from 'node:fs'
+import { createMiddleware } from 'hono/factory'
 import { logger } from 'hono/logger'
 import { Hono } from 'hono/quick'
 
@@ -9,7 +11,21 @@ app.onError((err, c) => {
   console.error(err)
   return c.text('An unexpected error occurred', 500)
 })
-
+// create temp dir
+app.use(
+  // biome-ignore lint/style/useNamingConvention:
+  createMiddleware<{ Variables: { out: string } }>(async (c, next) => {
+    // create temp dir
+    const out = mkdtempSync('out-')
+    try {
+      c.set('out', out)
+      await next()
+    } finally {
+      // remove temp dir
+      rmSync(out)
+    }
+  }),
+)
 app.post('/png', async c => {
   // get text
   const text = await c.req.text()
